3c1ee92bdecba7d3760cf6c2e18e38c312e12849
sadegh akbari <akbarisadegh382@gmail.com>
2024-07-28 14:35:11 +0330

    feat added scroll to top component

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Footer, LoginModal, Navbar } from "./components";
 import { Home, Cart, OrderPlace } from "./pages";
 import { useState } from "react";
+import { ScrollToTop } from "./utils";
 
 const App = () => {
   const [showLoginModal, setShowLoginModal] = useState(false);
@@ -11,6 +12,7 @@ const App = () => {
       {showLoginModal && <LoginModal setShowLoginModal={setShowLoginModal} />}
       <div className="app">
         <Navbar setShowLoginModal={setShowLoginModal} />
+        <ScrollToTop />
         <Routes>
           <Route path="*" element={<>not found</>} />
           <Route path="/" element={<Home />} />
